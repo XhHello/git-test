635609a9ed1adff435adad7ec03c896b615ac01e
FrankXu <Frank@chinanuicom.cn>
2022-11-22 11:13:51 +0800

    first commit

---
+++ src/main/java/git.java
@@ -0,0 +1,9 @@
+public class git {
+    public static void main(String[] args) {
+        System.out.println("这是原始代码");
+        System.out.println("原始代码上第一次修改");
+        System.out.println("原始代码上第二次修改");
+        System.out.println("原始代码上第三次修改");
+        System.out.println("这是push test");
+    }
+}
